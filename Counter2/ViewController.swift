188b9fbf98d332772ef831c3666afffe2dc49925
Maria Seliverstova <seliverstova.m@gmail.com>
2023-02-21 13:18:27 +0300

    Commit

--- Counter2/ViewController.swift
+++ Counter2/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ var totalCounts = 0
+    
+    @IBOutlet weak var countResults: UILabel!
+    @IBOutlet weak var countButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countButton.tintColor = .purple
     }
 
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        totalCounts += 1
+        countResults.text = String(totalCounts)    }
 }
 
